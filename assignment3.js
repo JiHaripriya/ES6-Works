f55d4f5c9effb578830284f95eace144849afe68
Haripriya <haripriya@qburst.com>
2020-10-19 20:14:02 +0530

    Assignment 3 questions

---
+++ assignment3.js
@@ -0,0 +1,27 @@
+const teams = [
+    {
+      name: "Chelsea",
+      id: 1,
+      groupId: [1, 2],
+      points: 10,
+    },
+    {
+      name: "Liverpool",
+      id: 2,
+      groupId: [2, 3],
+      points: 20,
+    },
+    {
+      name: "Manchester United",
+      id: 3,
+      groupId: [3, 4],
+      points: 50,
+    },
+    {
+      name: "Arsenal",
+      id: 4,
+      groupId: [1, 4],
+      points: 20,
+    },
+]
+  
